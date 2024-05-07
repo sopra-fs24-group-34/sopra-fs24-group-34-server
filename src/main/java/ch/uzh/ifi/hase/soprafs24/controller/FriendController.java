0e125bfe945aad89e4f6de18cd5bfb3b9a868231
Liam Kontopulos <liam.kontopulos@gmail.com>
2024-05-07 13:47:16 +0200

    Implemented sending and answering friend requests, commented out not implemented functions, and implementend get all friends function

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/FriendController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/FriendController.java
@@ -1,67 +1,82 @@
 package ch.uzh.ifi.hase.soprafs24.controller;
 
-import ch.uzh.ifi.hase.soprafs24.repository.UserRepository;
-import ch.uzh.ifi.hase.soprafs24.rest.dto.FriendRequestDTO;
+import ch.uzh.ifi.hase.soprafs24.entity.FriendRequest;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.FriendGetDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.FriendRequestPostDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.FriendRequestPutDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs24.service.FriendService;
+import ch.uzh.ifi.hase.soprafs24.entity.User;
+import ch.uzh.ifi.hase.soprafs24.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 public class FriendController {
 
     private final FriendService friendService;
+    private final UserService userService;
 
     @Autowired
-    FriendController(FriendService friendService, UserRepository userRepository) {
+    FriendController(FriendService friendService, UserService userService) {
         this.friendService = friendService;
+        this.userService = userService;
     }
 
 
     // add friend request
     @PostMapping("/users/{userId}/friends/add")
     @ResponseStatus(HttpStatus.CREATED)
-    public void addFriend(@PathVariable Long userId, @RequestBody FriendRequestDTO friendRequestDTO) {
+    public void sendFriendRequest(@PathVariable Long userId, @RequestBody FriendRequestPostDTO friendRequestPostDTO) {
+        User sender = userService.getUser(userId);
+        User receiver = userService.getUserByUsername(friendRequestPostDTO.getReceiverUserName());
+        FriendRequest friendRequest = DTOMapper.INSTANCE.convertFriendRequestPostDTOtoEntity(friendRequestPostDTO);
+        friendRequest.setReceiverId(receiver.getId());
+        friendRequest.setSenderUserName(sender.getUsername());
+        friendService.createFriendRequest(friendRequest);
     }
 
 
-    // Handle friend request
+    // Handle friend request (accept or decline)
     @PostMapping("/users/{userId}/friends/answer")
     @ResponseStatus(HttpStatus.OK)
-    public void answerFriendRequest(@PathVariable Long userId, @RequestBody FriendRequestDTO friendRequestDTO){
+    public void answerFriendRequest(@PathVariable Long userId, @RequestBody FriendRequestPutDTO friendRequestPutDTO){
+        FriendRequest friendrequest = DTOMapper.INSTANCE.convertFriendRequestPutDTOtoEntity(friendRequestPutDTO);
+        friendService.answerFriendRequest(friendrequest, friendRequestPutDTO.isAnswer());
 
     }
 
     //Invite friend into game
-    @PostMapping("/game/invite/{userId}")
+    /*@PostMapping("/game/invite/{userId}")
     @ResponseStatus(HttpStatus.CREATED)
     public void gameInvitation(@PathVariable Long userId, @RequestBody FriendRequestDTO gameInvitationDTO) {
 
-    }
+    }*/
 
     // Handle game invitation
-    @PostMapping("/game/{userId}/invitationresponse")
+    /*@PostMapping("/game/{userId}/invitationresponse")
     @ResponseStatus(HttpStatus.OK)
     public void handleGameInvitation(@PathVariable Long userId, @RequestBody FriendRequestDTO friendRequestDTO){
 
-    }
+    }*/
 
     // Delete friend
-    @PutMapping("/users/{userId}/friends/delete")
+    @DeleteMapping("/users/{userId}/friends/delete")
     @ResponseStatus(HttpStatus.OK)
-    public void deleteFriend(@PathVariable Long userId, @RequestParam("FriendId") Long friendId) {
+    public void deleteFriend(@PathVariable Long userId) {
 
     }
 
-    /*
-    get all friends
-     */
+    // Get all friends. (Only return Id, username and usericon)
     @GetMapping("/users/{userId}/friends")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public void getAllUsers(@PathVariable Long userId, @RequestHeader("Authorization") String authorization) {
-
+    public List<FriendGetDTO> getAllUsers(@PathVariable Long userId) {
+        User user = userService.getUser(userId);
+        return friendService.getFriends(user);
     }
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/FriendService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/FriendService.java
@@ -1,12 +1,21 @@
 package ch.uzh.ifi.hase.soprafs24.service;
 
+import ch.uzh.ifi.hase.soprafs24.entity.FriendRequest;
+import ch.uzh.ifi.hase.soprafs24.entity.User;
+import ch.uzh.ifi.hase.soprafs24.repository.FriendRequestRepository;
 import ch.uzh.ifi.hase.soprafs24.repository.LobbyRepository;
 import ch.uzh.ifi.hase.soprafs24.repository.UserRepository;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.FriendGetDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -14,11 +23,42 @@ public class FriendService {
 
     private final UserRepository userRepository;
     private final LobbyRepository lobbyRepository;
+    private final FriendRequestRepository friendRequestRepository;
 
     @Autowired
-    public FriendService(@Qualifier("userRepository") UserRepository userRepository, @Qualifier("lobbyRepository") LobbyRepository lobbyRepository) {
+    public FriendService(@Qualifier("userRepository") UserRepository userRepository, @Qualifier("lobbyRepository") LobbyRepository lobbyRepository,
+                         @Qualifier("friendRequestRepository") FriendRequestRepository friendRequestRepository){
         this.userRepository = userRepository;
         this.lobbyRepository = lobbyRepository;
+        this.friendRequestRepository = friendRequestRepository;
     }
 
+    public void createFriendRequest(FriendRequest friendRequest){
+        User sender = userRepository.findUserById(friendRequest.getSenderId());
+        User receiver = userRepository.findUserById(friendRequest.getReceiverId());
+        sender.addFriendRequest(friendRequest);
+        friendRequestRepository.save(friendRequest);
+        friendRequestRepository.flush();
+    }
+
+    public void answerFriendRequest(FriendRequest friendRequest, boolean answer){
+        User sender = userRepository.findUserById(friendRequest.getSenderId());
+        if (!sender.getFriendRequests().contains(friendRequest)) {
+            throw new IllegalArgumentException("This friend request does not exist");
+        }
+        sender.getFriendRequests().remove(friendRequest);
+        if (answer) {
+            User receiver = userRepository.findUserById(friendRequest.getReceiverId());
+            sender.addFriend(receiver);
+        }
+    }
+
+    public List<FriendGetDTO> getFriends(User user) {
+        Set<User> friends = user.getFriendsList();
+        List<FriendGetDTO> friendGetDTOs = new ArrayList<>();
+        for (User friend : friends) {
+            friendGetDTOs.add(DTOMapper.INSTANCE.convertEntityToFriendGetDTO(friend));
+        }
+        return friendGetDTOs;
+    }
 }
