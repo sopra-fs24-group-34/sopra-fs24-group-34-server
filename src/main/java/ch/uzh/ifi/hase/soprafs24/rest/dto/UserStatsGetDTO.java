0a51e8e02db34e2dc197458183c10084905879e7
smailalijagic <smail.alijagic@uzh.ch>
2024-05-21 16:10:48 +0200

    smailalijagic: resolves #180, some code clean up --> commented all unused code for DTO

--- src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/UserStatsGetDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/UserStatsGetDTO.java
@@ -7,30 +7,29 @@ import ch.uzh.ifi.hase.soprafs24.entity.User;
 import java.util.List;
 
 public class UserStatsGetDTO {
-  // smailalijagic: all updatable data
-  private String username;
+    // smailalijagic: all updatable data
+    private String username;
 
-  private UserStatus status;
+    private UserStatus status;
 
-  private String profilePicture; // smailalijagic: check this again --> String correct datatype?
+    private String profilePicture; // smailalijagic: check this again --> String correct datatype?
     
-  private Long id;
+    private Long id;
 
-  private Long totalplayed; // smailalijagic: natural number in range [0, n)
+    private Long totalplayed; // smailalijagic: natural number in range [0, n)
 
-  private Long totalwins; // smailalijagic: natural number in range [0, n)
+    private Long totalwins; // smailalijagic: natural number in range [0, n)
 
+    public String getUsername() {
+        return username;
+    }
 
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public String getProfilePicture() {
-        return profilePicture;
+      return profilePicture;
     }
 
     public void setProfilePicture(String profilePicture) {
@@ -38,36 +37,36 @@ public class UserStatsGetDTO {
     }
 
 
-  public Long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public UserStatus getStatus() {
-    return status;
-  }
+    public UserStatus getStatus() {
+        return status;
+    }
 
-  public void setStatus(UserStatus status) {
-    this.status = status;
-  }
+    public void setStatus(UserStatus status) {
+        this.status = status;
+    }
 
 
-  public Long getTotalplayed() {
-    return totalplayed;
-  }
+    public Long getTotalplayed() {
+        return totalplayed;
+    }
 
-  public void setTotalplayed(Long totalplayed) {
-    this.totalplayed = totalplayed;
-  }
+    public void setTotalplayed(Long totalplayed) {
+        this.totalplayed = totalplayed;
+    }
 
-  public Long getTotalwins() {
-    return totalwins;
-  }
+    public Long getTotalwins() {
+        return totalwins;
+    }
 
-  public void setTotalwins(Long totalwins) {
-    this.totalwins = totalwins;
-  }
+    public void setTotalwins(Long totalwins) {
+        this.totalwins = totalwins;
+    }
 }
