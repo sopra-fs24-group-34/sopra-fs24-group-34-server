5b3921ef513e8cf44a82c78619a38f02d4d497f1
smailalijagic <smail.alijagic@uzh.ch>
2024-05-23 09:56:43 +0200

    smailalijagic: resolves #180 removed all unused imports and variables. resolves #182

--- src/test/java/ch/uzh/ifi/hase/soprafs24/entity/ChatTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/entity/ChatTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ChatTest {
-
     @Test
     public void testSetMessagesAndGetMessages() {
         // Create a new chat instance
--- src/test/java/ch/uzh/ifi/hase/soprafs24/entity/LobbyInvitationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/entity/LobbyInvitationTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LobbyInvitationTest {
-
     @Test
     public void testSetCreatorUsernameSuccess() {
         // Create a LobbyInvitation instance
